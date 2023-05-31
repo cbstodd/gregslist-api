18bbccd7f0d3bd3cfa2f9f476a3c49efa8a27042
cbstodd <cbstodd@gmail.com>
2023-05-31 10:59:24 -0400

    Now able to post body data via bodyParser to new Items and Users

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const userRoutes = require('./api/routes/users');
 const itemRoutes = require('./api/routes/items');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/users', userRoutes);
 app.use('/items', itemRoutes);
+
+app.use(function (req, res, next) {
+  const error = new Error('Not Found!');
+  error.status = 404;
+  next(error);
+});
+
+app.use(function (error, req, res, next) {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
