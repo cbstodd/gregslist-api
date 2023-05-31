0c922249b68d7d15b6aecaaead8a4d7aab139e8c
cbstodd <cbstodd@gmail.com>
2023-05-31 14:15:31 -0400

    Added CORS permissions

--- app.js
+++ app.js
@@ -5,13 +5,32 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./api/routes/users');
 const itemRoutes = require('./api/routes/items');
 
+// Logging
 app.use(morgan('dev'));
+// Parsing data to be JSON.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({
+      message: 'OK',
+    });
+  }
+});
+
+// Routes which should handle requests
 app.use('/users', userRoutes);
 app.use('/items', itemRoutes);
 
+// Error handling
 app.use(function (req, res, next) {
   const error = new Error('Not Found!');
   error.status = 404;
